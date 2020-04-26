c0ad4f8f965b980e7d95cc1a429350c6871bae15
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-26 13:44:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,12 +1,11 @@
 require 'pry'
 
-def generate_star_date
-  star_date = (rand(100000) + 400000) / 10.0
-end
+
+star_date = (rand(100000) + 400000) / 10.0
 
 
 def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+  p "Captain's Log, star date #{star_date}."
 end
 
 def engage
