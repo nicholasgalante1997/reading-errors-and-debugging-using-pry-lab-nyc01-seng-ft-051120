d357a5b90503607dfd11ccadef648cff3be2e40c
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-26 13:47:10 +0000

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,7 +1,8 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def get_user_input
-  gets.chomp
+  raw_input = gets.chomp
+  num = raw_input.to_i 
 end
 
 def prompt_user
@@ -11,10 +12,11 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
+   else 
   end
 end
 
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,9 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    new_string = "ssssssssss#{string}"
+    new_string
   else
     string
   end
