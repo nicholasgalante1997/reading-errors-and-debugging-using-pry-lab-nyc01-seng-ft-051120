534413e7d61008b298973bab417900aebc620540
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-26 13:17:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
-def snake_it_up(string = [])
+def snake_it_up(string)
   if string[0] == "s"
     10.times do 
-      string << "s"
+      string.unshift("s")
     end 
   else
     string
